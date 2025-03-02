7f5450309c4dff1329225a54daf69e5f7d130b05
balianxp <kickjamptfm@gmail.com>
2025-03-02 13:25:54 -0300

    Atualizado token do bot

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ logger = logging.getLogger(__name__)
 
 # Constantes
 class Config:
-    TOKEN = "7633548560:AAGOK4Hc5kn6BLIAL9VrEaH_CwsRp69hU_0"  # Token do bot
+    TOKEN = "7136668085:AAGWw9gIgQCZbb5bunB2ZawRsHY3Dt8BrPs"  # Token do bot
     ADMIN_ID = 6206033489  # Seu ID no Telegram
     VIP_GROUP = "https://t.me/+qYv4s2q0AQBjYTVh"  # Grupo VIP
     PREVIEW_GROUP = "https://t.me/lulupriminha"   # Grupo de prévias
